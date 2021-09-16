64c8c8865245e60091e064a56ec33bc54cae3e5e
Tushar Kaundal <kaundaltushar@gmail.com>
2021-09-17 02:02:59 +0530

    diplayed game history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
 import { calculateWinner } from './helper';
+import History from './components/History ';
 import './styles/root.scss';
 
 const App = () => {
@@ -36,12 +37,15 @@ const App = () => {
     });
     setCurrentMove(prev => prev + 1);
   };
-
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
   return (
     <div className="app">
       <h1>TIC TACK TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
