c5cf6d3c0efe4187e87b96aae0ae17eabe0564c6
Tushar Kaundal <kaundaltushar@gmail.com>
2021-09-16 23:32:53 +0530

    added some events

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, clickHandler }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={clickHandler}>
       {value}
     </button>
   );
